b268a0e87fd24a517a841c091d535f12226176e4
joansalto <joansalto14@gmail.com>
2018-04-11 00:04:36 +0200

    Update install.sh

--- OBD/install.sh
+++ OBD/install.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+#OBD
 apt-get update
 apt-get upgrade
 pip install obd
 apt-get install bluetooth bluez blueman
+
+#GPS
+apt-get install pps-tools
+apt-get install libcap-dev
+apt-get install gpsd gpsd-clients python-gps
+apt-get update
+
+cp -vuf ./config.txt /boot/config.txt
+cp -vuf ./modules /etc/moduels
+
+#No estoy seguro si esto funciona sin reboot
+#reboot
+cp -vuf ./gpsd /etc/default/gpsd
+
+sudo gpsd /dev/ttyAMA0 -F /var/run/gpsd-sock
+#cgps -s
