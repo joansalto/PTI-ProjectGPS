43c8a4cf2c12d1d0893edf9bad68786361799561
wanetty <eduard.gonzalez.moreno@est.fib.upc.edu>
2018-04-16 20:40:25 +0200

    Escritura & Lectura local ODB
    Añado una función para escribir en local en caso de fallo de conexión y otra función en caso de recuperarla leer y enviar.
    Además creo una minifunción para poder escribir facilmente los logs.

--- OBD/connectOBD.py
+++ OBD/connectOBD.py
@@ -1,29 +1,73 @@
 #!/bin/sh
 
-import obd, os, datetime, time, os, json, sys, logging
+import obd, os, datetime, time, os, json, sys, logging,csv,os.path
 from obd import OBDStatus
 from time import time
 
-
-tiempo = 0
-speed = obd.commands.SPEED # select an OBD command (sensor)
-rpm = obd.commands.RPM
-fuel_lvl = obd.commands.FUEL_LEVEL
-run_time = obd.commands.RUN_TIME
-distance = obd.commands.DISTANCE_W_MIL
+########################VARIABLES GLOBALES#########################
+url = "http://54.149.160.242/items"								  #
+tiempo = 0														  # 
+speed = obd.commands.SPEED # select an OBD command (sensor)       #
+rpm = obd.commands.RPM											  #
+fuel_lvl = obd.commands.FUEL_LEVEL								  #
+run_time = obd.commands.RUN_TIME								  #
+distance = obd.commands.DISTANCE_W_MIL							  #
+###################################################################
 
 def init_config():
 	obd.logger.removeHandler(obd.console_handler)
 	sys.tracebacklimit = 0 #Te quita el traceback de los errores.Comentar para errores
+	
+def writelog(mensaje):
+	NOW = datetime.datetime.now()
+	f=open("log.txt","a")
+	f.write("["+str(NOW)[0:19] + "] "+mensaje+"\n")
+	f.close()	
 
-def send_data(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
-	json_data = {"data": str(ahora), "speed": data_speed,"rpm":data_rpm, "lvl":data_lvl, "time":data_time, "distance": data_distance}
-	url = "http://54.149.160.242:80"
+def write_csv(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
+	myFile = open('datosODB.csv', 'a')
+	datos = [[str(ahora)[0:19],data_speed,data_rpm, data_lvl, data_time, data_distance]]
+	with myFile:
+		writer = csv.writer(myFile)
+		writer.writerows(datos)
+		myFile.close()
+		writelog("Datos en local guardados correctamente.")
+
+
+def send(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
+	json_data = {"data": str(ahora)[0:19], "speed": data_speed,"rpm":data_rpm, "lvl":data_lvl, "time":data_time, "distance": data_distance}
+	print json_data #comentario
 	headers = {'Content-type': 'application/json'}
 	try:
 		r = requests.post(url, data=json.dumps(json_data), headers=headers)
+		return 0
 	except:
-		print "Fallo de conexion"
+		return -1
+		
+def read_and_send():
+	#Primero miro si exoiste el fichero para saber si hay datos que enviar.  SI hay datos los leo y envio.
+	#En el momento que se corte la comunicacion dejo de leer y elimino el fichero.
+	if os.path.isfile("datosODB.csv"): 
+		with open('datosODB.csv') as File:
+			reader = csv.reader(File, delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
+			result = -1
+			for row in reader:
+				result = send(row[0],row[1],row[2],row[3],row[4],row[5]) #enviamos los datos de la fila
+				if result == -1:
+					File.close()
+					break
+			if result == 0:
+				os.system("rm datosODB.csv")
+				writelog("Fichero borrado correctamente.")
+	else :
+		writelog("Sin datos en local")		
+
+def send_data(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
+	result = send(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance)
+	if result == -1: #Hay error por lo que escribirmos en csv.
+		write_csv(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance)
+	else :
+		read_and_send() #Al no haber error leemos si hay archivo y si lo hay enviamos datos.
 
 def comprobar_conexion():
 	ahora = datetime.datetime.now()
@@ -31,9 +75,11 @@ def comprobar_conexion():
 		print ("[%s] ELM conectado, falta arrancar el coche" %ahora)
 		return 0
 	elif connection.status() == OBDStatus.NOT_CONNECTED:
+		writelog("Estado: No conectado.")
 		print ("[%s] No conectado" %ahora)
 		return -1
 	else :
+		writelog("Conectado y recogiendo datos.")
 		print ("[%s] Conectado y recogiendo datos!" %ahora)
 		return 1
 
@@ -61,16 +107,16 @@ while (estado == 1):
 			print ("[%s] Conexion perdida" %ahora)
 			break
 		ahora = datetime.datetime.now()
-		print ("[%s]" %ahora)
 		data_speed = imprimir_dato(speed)
 		data_rpm = imprimir_dato(rpm)
 		data_lvl = imprimir_dato(fuel_lvl)
 		data_time = imprimir_dato(run_time)
 		data_distance = imprimir_dato(distance)
+		send_data(ahora,data_speed, data_rpm, data_lvl, data_time, data_distance)
 		tiempo = 0
 		start_time = time()
 			
 if (estado == -1):
 	ahora = datetime.datetime.now()
-	print ("[%s] Imposible conectar, finalizando programa" %ahora)
+	writelog("Imposible conectar, finalizando el programa")
 exit 
--- OBD/test.py
+++ OBD/test.py
@@ -1,54 +1,72 @@
 #!/bin/sh
 
 
-import datetime, time, os, json, requests, logging,sys 
+import datetime, time, os, json, requests, logging,sys, csv, os.path
 from time import time
 import simplejson as json
 
-def send_data(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
-	json_data = {"data": str(ahora), "speed": data_speed,"rpm":data_rpm, "lvl":data_lvl, "time":data_time, "distance": data_distance}
-	print json_data
-	url = "http://54.149.160.242:80"
+def writelog(mensaje):
+	NOW = datetime.datetime.now()
+	f=open("log.txt","a")
+	f.write("["+str(NOW)[0:19] + "] "+mensaje+"\n")
+	f.close()
+
+def write_csv(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
+	myFile = open('datosODB.csv', 'a')
+	datos = [[str(ahora)[0:19],data_speed,data_rpm, data_lvl, data_time, data_distance]]
+	with myFile:
+		writer = csv.writer(myFile)
+		writer.writerows(datos)
+		myFile.close()
+		writelog("Datos guardados en local")
+		print ("Escritura completa")
+
+def send(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
+	json_data = {"data": str(ahora)[0:19], "speed": data_speed,"rpm":data_rpm, "lvl":data_lvl, "time":data_time, "distance": data_distance}
+	print json_data #comentario
+	url = "http://54.149.160.242/items"
 	headers = {'Content-type': 'application/json'}
 	try:
 		r = requests.post(url, data=json.dumps(json_data), headers=headers)
+		return 0
 	except:
-		print "Fallo de conexion"
+		return -1
 
-#######################################MAIN#######################################3
-mensaje = "hola"
+def read_and_send():
+	#Primero miro si exoiste el fichero para saber si hay datos que enviar.  SI hay datos los leo y envio.
+	#En el momento que se corte la comunicacion dejo de leer y elimino el fichero.
+	if os.path.isfile("datosODB.csv"): 
+		with open('datosODB.csv') as File:
+			reader = csv.reader(File, delimiter=',', quotechar=',',quoting=csv.QUOTE_MINIMAL)
+			result = -1
+			for row in reader:
+				result = send(row[0],row[1],row[2],row[3],row[4],row[5]) #enviamos los datos de la fila
+				writelog("Enviando datos de local")
+				if result == -1:
+					File.close()
+					break
+			if result == 0:
+				os.system("rm datosODB.csv")
+				writelog("Fichero borrado correctamente.")
+				print ("Fichero enviado y borrado")
+	else :
+		writelog("Sin datos a enviar")
+		print("Documento no existente")
+
+
+def send_data(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance):
+	result = send(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance)
+	if result == -1: #Hay error por lo que escribirmos en csv.
+		write_csv(ahora, data_speed, data_rpm, data_lvl, data_time, data_distance)
+	else :
+		read_and_send() #Al no haber error leemos si hay archivo y si lo hay enviamos datos.
+
+#######################################MAIN#######################################
 sys.tracebacklimit = 0 #Te quita el traceback de los errores.
 data_speed = 10
-data_rpm = 2
-data_lvl = 3
-data_time = 4
-data_distance = 5 
+data_rpm = 5
+data_lvl = 47
+data_time = 58
+data_distance = 9 
 ahora = datetime.datetime.now()
 send_data(ahora,data_speed,data_rpm,data_lvl,data_time, data_distance)
-
-
-	
-
-
-exit
-
-def say_hola():
-	print mensaje
-
-	
-start_time= time()
-tiempo = 0
-n = 0
-"""while n < 20000000:
-	n = n + 1
-	say_hola()
-	end_time = time()
-	tiempo = end_time - start_time 
-	print str(tiempo)
-	#os.system('clear')
-	if tiempo > 2:
-		tiempo = 0
-		ahora = datetime.datetime.now()
-		
-		print ("[%s] " %ahora) + str(n)
-		start_time = time()"""
