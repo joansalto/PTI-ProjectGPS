10b59ae85e727033137bcc9cc1219769bc75a879
joansalto <joansalto14@gmail.com>
2018-05-06 20:36:07 +0200

    Add files via upload

--- webapp/app.js
+++ webapp/app.js
@@ -3,13 +3,13 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var session = require("express-session");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var holaMundo = require('./routes/holaMundo');
 var login = require('./routes/login');
 var apiRest = require('./routes/ApiRest');
+var client = require('./routes/client');
 var app = express();
 
 // view engine setup
@@ -21,18 +21,14 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-    secret: 'abcd1234',
-    resave: false,
-    saveUninitialized: false
-
-}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use ('/holaMundo', holaMundo);
 app.use('/login', login);
 app.use('/apiRest', apiRest);
+app.use('/client', client);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
