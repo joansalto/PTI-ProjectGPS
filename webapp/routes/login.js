4cb9d159f6d421e66511ea4c8eccb156e73eecbf
wanetty <eduard.gm@gmail.com>
2018-05-06 16:59:09 +0200

    Cambio login.

--- webapp/routes/login.js
+++ webapp/routes/login.js
@@ -1,8 +1,22 @@
 var express = require('express');
+var conn = require ('../models/connection');
+var mysql = require('mysql'),
+    connection = mysql.createConnection(
+        conn
+    );
+
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
     res.render('login');
+});
+router.post('/', function(req, res, next) {
+    var usuario = req.body.user;
+    var pass = req.body.password;
+    console.log("usuario: " + usuario);
+    console.log("password: " + pass);
+    connection.query
+    res.end();
 });
 
 module.exports = router;
