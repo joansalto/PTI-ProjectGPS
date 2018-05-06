29bbfdf2a56abba44000a475f1d8e599efa834a4
DESKTOP-O3MIS3T\Micky <micky101224@gmail.com>
2018-05-06 18:57:33 +0200

    login hecho falta testeo

--- webapp/routes/login.js
+++ webapp/routes/login.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var conn = require ('../models/connection');
+var md5 = require('md5');
 var mysql = require('mysql'),
+
     connection = mysql.createConnection(
         conn
     );
@@ -11,12 +13,33 @@ router.get('/', function(req, res, next) {
     res.render('login');
 });
 router.post('/', function(req, res, next) {
+
     var usuario = req.body.user;
     var pass = req.body.password;
     console.log("usuario: " + usuario);
     console.log("password: " + pass);
-    connection.query
+    var cifrado = md5(pass);
+    connection.query('select * from users where user = "'+usuario+'"  and pass = "'+cifrado+'"', function (err, result) {
+        if (err) throw err;
+        console.log(result);
+        if(result.length > 0) {
+            req.session.logueado = 1;
+        }
+    });
+    if (req.session.logueado){
+        /*res.writeHead(301,
+            {Location: 'http://localhost/menu'}
+        );
+        res.end();
+       */
+        console.log('logueado con exito');
+
+    }
+    else { console.log("Tu puta mae guea")}
+
+
     res.end();
+
 });
 
 module.exports = router;
