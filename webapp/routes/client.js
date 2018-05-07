0c8b7f39a557e7a0c3db323a9b834648c8173e22
joansalto <joansalto14@gmail.com>
2018-05-07 19:04:04 +0200

    Client new user

--- webapp/routes/client.js
+++ webapp/routes/client.js
@@ -20,3 +20,23 @@ router.get('/', function(req, res, next) {
 
 module.exports = router;
 
+router.post('/', function(req, res, next){
+
+    var sql = 'INSERT INTO ClientData VALUE ?'
+    var dni = req.dni;
+    var nombre = req.nombre;
+    var apellido = req.apellido;
+    var telefono = req.telefono;
+    var email = req.mail;
+    var fechaNacimineto = req.fechaNacimiento;
+
+    var values = dni + "," + nombre + "," + apellido + "," + telefono + "," + email + "," + fechaNacimineto;
+
+    //falta hacer la llamada y lo que hara
+    connection.query(sql,values, function(err) {
+        if (err) throw err;
+
+    });
+}
+
+
