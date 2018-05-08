739f69df77853d6ef3f228aeaea972389089ba2a
muniategui <jdemuniategui@hotmail.com>
2018-05-08 23:12:35 +0200

    On progres 1

--- webapp/routes/rasp.js
+++ webapp/routes/rasp.js
@@ -8,13 +8,18 @@ var mysql = require('mysql'),
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-
+    if(req.session.logueado === 0) {
+        res.writeHead(301,
+            {Location: './'}
+        );
+        setTimeout(res.end(), 3000);
+    }
     var sql = 'SELECT RaspID,DNI from Rasps left join ClientData on ClientID=ID';
 
 
     connection.query(sql, function(err, rows, fields) {
         if (err) throw err;
-        res.render('rasp', { title: 'Raspberrys', rows: rows });
+        res.render('rasp', { title: 'Raspberrys', menu: "raspberry", rows: rows });
     });
 });
 
