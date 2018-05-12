5f43403726e5096992c41adc515492730b57aabb
Muniategui <jdemuniategui@hotmail.com>
2018-05-12 12:15:24 +0200

    Allowed to delete, desasociate and asociate

--- webapp/routes/rasp.js
+++ webapp/routes/rasp.js
@@ -19,9 +19,29 @@ router.get('/', function(req, res, next) {
 
     connection.query(sql, function(err, rows, fields) {
         if (err) throw err;
-        res.render('rasp', { title: 'Raspberrys', menu: "raspberry", rows: rows });
+        var sql2 = 'SELECT DNI from ClientData';
+        connection.query(sql2, function(err, DNIS, fields) {
+            if (err) throw err;
+            res.render('rasp', { title: 'Raspberrys', menu: "raspberry", rows: rows, DNIS:DNIS});
+        });
     });
 });
+// router.get('/DNI', function(req, res, next) {
+//     if(req.session.logueado === 0) {
+//         res.writeHead(301,
+//             {Location: './'}
+//         );
+//         setTimeout(res.end(), 3000);
+//     }
+//     var sql = 'SELECT DNI from ClientData';
+//
+//
+//     connection.query(sql, function(err, DNIS, fields) {
+//         if (err) throw err;
+//         res.render('rasp', { title: 'Raspberrys', menu: "raspberry", DNIS:DNIS });
+//
+//     });
+// });
 
 module.exports = router;
 
@@ -34,6 +54,39 @@ router.post('/desasociar', function(req, res, next){
         if (err) throw err;
 
     });
+    res.end();
+
+});
+router.post('/asociar', function(req, res, next){
+    var RaspID = req.body.RaspID;
+    var DNI = req.body.DNI;
+    console.log(RaspID);
+    console.log(DNI);
+    var sql = 'UPDATE Rasps SET ClientID = (SELECT ID FROM ClientData WHERE DNI ="'+DNI+'") WHERE RaspID="'+RaspID+'"';
+    connection.query(sql, function (err, result) {
+        console.log(sql);
+        if (err) throw err;
+
+    });
+    res.end();
+
+});
+router.post('/eliminar', function(req, res, next){
+    var RaspID = req.body.RaspID;
+    console.log(RaspID);
+    var sql = 'DELETE FROM CarData WHERE ID_rasp ="'+RaspID+'"';
+    connection.query(sql, function (err, result) {
+        console.log(sql);
+        if (err) throw err;
+
+    });
+    var sql = 'DELETE FROM Rasps WHERE RaspID ="'+RaspID+'"';
+    connection.query(sql, function (err, result) {
+        console.log(sql);
+        if (err) throw err;
+
+    });
+
     res.end();
 
 });
