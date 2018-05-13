298ea525cb1872d0f17ea4d1c54638b9444099d4
wanetty <eduard.gm@gmail.com>
2018-05-13 20:06:50 +0200

    Modifico apirest

--- webapp/routes/ApiRest.js
+++ webapp/routes/ApiRest.js
@@ -15,31 +15,29 @@ router.get('/', function(req, res) {
 
 /* Falta modificar para el funcionamiento correcto. */
 router.post('/', function(req, res) {
-
-    var obj = req.body;
     var data = req.body.data;
     var speed = req.body.speed;
     var rpm = req.body.rpm;
     var lvl =  req.body.lvl;
     var time = req.body.time;
     var distance = req.body.distance;
-
-//    var idrasp = req.body.idrasp;
-//    var x = req.body.x;
-//    var y = req.body.y;
-    console.log(data+" "+speed+" "+rpm+" "+lvl+" "+time+" "+distance);
-
-    var query = connection.query('INSERT INTO CarData (ID_rasp, Fecha, RPM, Speed, Fuel_lvl, Runtime, Distance, X, Y) VALUES ("0", "'+data+'" , '+rpm+', '+speed+','+lvl+', '+time+','+distance+',"test","test")', function(error, result){
-            if(error){
-                throw error;
-            }else{
-                console.log(result);
-            }
+    var idrasp = req.body.idrasp;
+    var x = req.body.x;
+    var y = req.body.y;
+    //console.log(data+" "+speed+" "+rpm+" "+lvl+" "+time+" "+distance);
+    connection.query('SELECT clientID FROM CarLocator.Rasps where RaspID = '+idrasp +' and clientID is not null',function(error, result){
+        if(error){
+            console.log(error);
+        }else{
+            connection.query('INSERT INTO CarData (ID_rasp, Fecha, RPM, Speed, Fuel_lvl, Runtime, Distance, X, Y, idCliente) VALUES ('+idrasp+', "'+data+'" , '+rpm+', '+speed+','+lvl+', '+time+','+distance+',"'+x+'","'+y+'",'+result[0].clientID+')', function(error, result){
+                if(error){
+                    console.log(error);
+                }else{
+                    //console.log(result);
+                }
+            });
         }
-    );
-
-
-
+    });
     res.end();
 
 
