7f34bf3436ac2da3868ae4a2e7f9a052d3b309a2
Eduard Gonzalez <eduard.gm@gmail.com>
2018-05-23 19:54:45 +0200

    Puesta apunto de la web.

--- webapp/routes/controlPanel.js
+++ webapp/routes/controlPanel.js
@@ -33,8 +33,6 @@ router.post('/', function(req, res) {
                 for(var i = 0; i < result.length; ++i){
                     var date = format(result[i].Fecha);
                     date = date.toString().substr(0,10) +" "+ date.toString().substr(11,8);
-                    console.log(result[i].X);
-                    console.log(result[i].Y);
                     if(!primero && result[i].X != 0 && result[i].Y != 0) {
                         marcadores  = "[["+result[i].X+","+result[i].Y+"],";
                         primero = true;
@@ -54,10 +52,14 @@ router.post('/', function(req, res) {
 
                     }
                 }
-                fechas = fechas.substr(0,(fechas.length -1));
-                velocidad = velocidad.substr(0,(velocidad.length -1));
-                rpm = rpm.substr(0,(rpm.length -1));
-                marcadores = marcadores.substr(0,(marcadores.length -1))+"]";
+                if(fechas != null) fechas = fechas.substr(0,(fechas.length -1));
+                else fechas = "";
+                if(velocidad != null) velocidad = velocidad.substr(0,(velocidad.length -1));
+                else velocidad = "";
+                if(rpm != null) rpm = rpm.substr(0,(rpm.length -1));
+                else rpm = "";
+                if(marcadores != null) marcadores = marcadores.substr(0,(marcadores.length -1))+"]";
+                else marcadores = "[[0,0]]";
                 sql="SELECT MAX(distance) AS dis, MAX(runtime) AS runtime,Fuel_lvl ,  Sesion  FROM  CarLocator.CarData WHERE  idCliente = "+idCliente+" GROUP BY Sesion";
                 connection.query(sql, function (err,result) {
                     var sesion, runtime, distance;
@@ -65,24 +67,26 @@ router.post('/', function(req, res) {
                         if (i === 0){
                             sesion = "'"+result[i].Sesion+"',";
                             runtime = (result[i].runtime/60) +",";
-                            distance = result[i].dis +",";
                             fuel = result[i].Fuel_lvl +",";
                         }
                         else {
                             sesion =sesion + "'"+result[i].Sesion+"',";
                             runtime =runtime+ (result[i].runtime/60) +",";
-                            distance =distance+  +result[i].dis +",";
                             fuel =fuel+  +result[i].Fuel_lvl +",";
                         }
+                        if(result.length == i+1) distance = result[i].dis;
                     }
-                    sesion = sesion.substr(0,(sesion.length -1));
-                    runtime = runtime.substr(0,(runtime.length -1));
-                    distance = distance.substr(0,(distance.length -1));
-                    fuel = fuel.substr(0,(fuel.length -1));
+                    if(sesion != null) sesion = sesion.substr(0,(sesion.length -1));
+                    else sesion = "";
+                    if(runtime != null) runtime = runtime.substr(0,(runtime.length -1));
+                    else runtime = "";
+                    if(fuel != null) fuel = fuel.substr(0,(fuel.length -1));
+                    else fuel = "";
+
                     sql = "SELECT distinct Sesion FROM CarData where  idCliente ="+idCliente;
                     connection.query(sql, function (err,result ) {
                         if(err) console.log(err);
-                        res.render(`controlPanel`,{infoCliente:infoCliente, DNI:DNI, datos:result, fechas:fechas, velocidades:velocidad, rpm:rpm, fuel:fuel, sesions:sesion,runtimes:runtime,distances:distance,selectSesions :result, select:Sesionselected, marcadores:marcadores});
+                        res.render(`controlPanel`,{infoCliente:infoCliente, DNI:DNI, datos:result, fechas:fechas, velocidades:velocidad, rpm:rpm, fuel:fuel, sesions:sesion,runtimes:runtime,distance:distance,selectSesions :result, select:Sesionselected, marcadores:marcadores});
                     })
 
                 });
--- webapp/routes/index.js
+++ webapp/routes/index.js
@@ -19,16 +19,15 @@ router.get('/', function(req, res, next) {
       var activos;
 
       connection.query('SELECT * FROM CarLocator.CarData GROUP BY ID_rasp', function (err, result){
-          if (err) throw err;
+          if (err) console.log(err);
           else totalrasp = result.length;
-          console.log(totalrasp);
           connection.query('UPDATE CarLocator.Estadisticas set rasps = '+totalrasp, function (err, result){
               if (err) console.log(err);
           });
 
       });
       connection.query('Select sum(d.dis) as dista from (SELECT MAX(Distance) as dis from CarLocator.CarData group by Sesion) d', function (err, result){
-          if (err) throw err;
+          if (err) console.log(err);
           else distance = result[0].dista;
           connection.query('UPDATE Estadisticas set distance = '+distance+'', function (err, result){
               if (err) console.log(err);
@@ -36,7 +35,7 @@ router.get('/', function(req, res, next) {
 
       });
       connection.query('Select avg(RPM) as rpm from CarLocator.CarData', function (err, result){
-          if (err) throw err;
+          if (err) console.log(err);
           else RPM = result[0].rpm;
          connection.query('UPDATE Estadisticas set rpm = '+RPM+'', function (err, result){
               if (err) console.log(err);
@@ -44,15 +43,15 @@ router.get('/', function(req, res, next) {
       });
 
       connection.query('Select sum(r.run) as runt from (SELECT MAX(Runtime) as run from CarLocator.CarData group by Sesion) r', function (err, result){
-          if (err) throw err;
-          else time = result[0].runt;
+          if (err) console.log(err);
+          else time = result[0].runt/60;
           connection.query('UPDATE Estadisticas set time = '+time+'', function (err, result){
               if (err) console.log(err);
           });
       });
 
       connection.query('SELECT Count(*) as total from Rasps where ClientID is not null', function (err, result){
-          if (err) throw err;
+          if (err) console.log(err);
           else activos = result[0].total;
           connection.query('UPDATE Estadisticas set activos = '+activos+'', function (err, result){
               if (err) console.log(err);
@@ -60,7 +59,7 @@ router.get('/', function(req, res, next) {
       });
 
       connection.query('SELECT count(DNI) as clients from ClientData', function (err, result){
-          if (err) throw err;
+          if (err) console.log(err);
           else totalclient = result[0].clients;
           connection.query('UPDATE Estadisticas set clients = '+totalclient+'', function (err, result){
               if (err) console.log(err);
@@ -69,12 +68,12 @@ router.get('/', function(req, res, next) {
 
       setTimeout(function () {
           connection.query('SELECT * from Estadisticas', function (err, result){
-              if (err) throw err;
+              if (err) console.log(err);
               else res.render('index', { title: 'Carlocator', menu:'principal', rasps:result[0].rasps, dist: result[0].distance, rpm: result[0].rpm, time: result[0].time, activos:result[0].activos,clients:result[0].clients });
 
           });
 
-      },3000);
+      },2000);
 
 
   }else{
